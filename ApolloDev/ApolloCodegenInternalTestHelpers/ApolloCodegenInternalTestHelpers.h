89f85bfa42944fbc1fae446788fe3f7cf375a98a
Zach FettersMoore <4425109+BobaFetters@users.noreply.github.com>
2023-06-08 14:30:42 -0400

    Added 3 missing targets
    Added targets for ApolloCodegenInternalTestHelpers, ApolloCodegenTests, and CodegenCLITests

---
+++ ApolloDev/ApolloCodegenInternalTestHelpers/ApolloCodegenInternalTestHelpers.h
@@ -0,0 +1,19 @@
+//
+//  ApolloCodegenInternalTestHelpers.h
+//  ApolloCodegenInternalTestHelpers
+//
+//  Created by Zach FettersMoore on 6/7/23.
+//  Copyright © 2023 Apollo GraphQL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ApolloCodegenInternalTestHelpers.
+FOUNDATION_EXPORT double ApolloCodegenInternalTestHelpersVersionNumber;
+
+//! Project version string for ApolloCodegenInternalTestHelpers.
+FOUNDATION_EXPORT const unsigned char ApolloCodegenInternalTestHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApolloCodegenInternalTestHelpers/PublicHeader.h>
+
+
