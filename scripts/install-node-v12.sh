0660a1e78b0ecd47e00ce6c3ff20a13b931f8a7a
Zach FettersMoore <4425109+BobaFetters@users.noreply.github.com>
2023-08-10 16:31:10 -0400

    Updating integration test script

--- scripts/install-node-v12.sh
+++ scripts/install-node-v12.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-touch $BASH_ENV
+# touch $BASH_ENV
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-echo 'export NVM_DIR="$HOME/.nvm"' >> $BASH_ENV
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $BASH_ENV
-echo nvm install v12.22.10 >> $BASH_ENV
-echo nvm use v18.15.0 >> $BASH_ENV
+echo 'export NVM_DIR="$HOME/.nvm"' >> $GITHUB_ENV
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $GITHUB_ENV
+echo nvm install v12.22.10 >> $GITHUB_ENV
+echo nvm use v18.15.0 >> $GITHUB_ENV
