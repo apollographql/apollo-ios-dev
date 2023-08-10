ba2185cfca153ba05611dc24996ad52ed2174def
Zach FettersMoore <4425109+BobaFetters@users.noreply.github.com>
2023-08-10 16:45:55 -0400

    Focusing on star wars server testing

--- scripts/install-node-v12.sh
+++ scripts/install-node-v12.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# touch $BASH_ENV
+touch $GITHUB_ENV
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 echo 'export NVM_DIR="$HOME/.nvm"' >> $GITHUB_ENV
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $GITHUB_ENV
