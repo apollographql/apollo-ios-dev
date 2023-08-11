3f0dcdd8ad90366331690e8d73e0aa6a4d713a29
Zach FettersMoore <4425109+BobaFetters@users.noreply.github.com>
2023-08-11 11:54:55 -0400

    Testing nvm commands

--- scripts/install-node-v12.sh
+++ scripts/install-node-v12.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -i
+#!/bin/bash
 
-touch $GITHUB_ENV
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-echo 'export NVM_DIR="$HOME/.nvm"' >> $GITHUB_ENV
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $GITHUB_ENV
+#touch $GITHUB_ENV
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+#echo 'export NVM_DIR="$HOME/.nvm"' >> $GITHUB_ENV
+#echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $GITHUB_ENV
 echo nvm install v12.22.10 >> $GITHUB_ENV
 echo nvm use v18.15.0 >> $GITHUB_ENV
