466e2595d5b36f1b88e886fd8254a2f1fe3cbc9f
Zach FettersMoore <4425109+BobaFetters@users.noreply.github.com>
2023-08-14 10:37:56 -0400

    Adding -v to swift test run

--- Tests/TestCodeGenConfigurations/EmbeddedInTarget-InSchemaModule/Tests/TestAppTests/TestAppTests.swift
+++ Tests/TestCodeGenConfigurations/EmbeddedInTarget-InSchemaModule/Tests/TestAppTests/TestAppTests.swift
@@ -4,39 +4,39 @@ import Apollo
 import ApolloTestSupport
 
 final class TestAppTests: XCTestCase {
-//  func testCacheKeyResolution() throws {
-//    let store = ApolloStore()
-//    
-//    let response = GraphQLResponse(
-//      operation: AnimalKingdomAPI.DogQuery(),
-//      body: ["data": [
-//        "allAnimals": [
-//          [
-//            "__typename": "Dog",
-//            "id": "1",
-//            "skinCovering": "Fur",
-//            "species": "Canine",
-//          ]
-//        ]
-//      ]])
-//    
-//    let (_, records) = try response.parseResult()
-//    
-//    let expectation = expectation(description: "Publish Record then Fetch")
-//    
-//    store.publish(records: records!) { _ in
-//      store.withinReadTransaction { transaction in
-//        let dog = try! transaction.readObject(
-//          ofType: AnimalKingdomAPI.DogQuery.Data.AllAnimal.self,
-//          withKey: "Dog:1")
-//        
-//        XCTAssertEqual(dog.id, "1")
-//        expectation.fulfill()
-//      }
-//    }
-//    
-//    waitForExpectations(timeout: 1.0)
-//  }
+  func testCacheKeyResolution() throws {
+    let store = ApolloStore()
+    
+    let response = GraphQLResponse(
+      operation: AnimalKingdomAPI.DogQuery(),
+      body: ["data": [
+        "allAnimals": [
+          [
+            "__typename": "Dog",
+            "id": "1",
+            "skinCovering": "Fur",
+            "species": "Canine",
+          ]
+        ]
+      ]])
+    
+    let (_, records) = try response.parseResult()
+    
+    let expectation = expectation(description: "Publish Record then Fetch")
+    
+    store.publish(records: records!) { _ in
+      store.withinReadTransaction { transaction in
+        let dog = try! transaction.readObject(
+          ofType: AnimalKingdomAPI.DogQuery.Data.AllAnimal.self,
+          withKey: "Dog:1")
+        
+        XCTAssertEqual(dog.id, "1")
+        expectation.fulfill()
+      }
+    }
+    
+    waitForExpectations(timeout: 1.0)
+  }
   
   func test_mockObject_initialization() throws {
     // given
