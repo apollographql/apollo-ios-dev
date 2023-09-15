8a73444958c6f8dcf6c1c0d12aeee8d6063f082e
Anthony Miller <anthonymdev@gmail.com>
2023-09-15 09:37:03 -0700

    Include local SPM packages in project, instead of as additional files (apollographql/apollo-ios-dev#34)

--- Workspace.swift
+++ Workspace.swift
@@ -4,11 +4,7 @@ let workspace = Workspace(
     name: "ApolloDev",
     projects: [
         "./"
-    ],
-    additionalFiles: [
-        .folderReference(path: "apollo-ios"),
-        .folderReference(path: "apollo-ios-codegen")
-    ],
+    ],    
     generationOptions: .options(
         enableAutomaticXcodeSchemes: true
     )
