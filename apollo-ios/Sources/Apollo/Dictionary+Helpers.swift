6592815585241eb696adab09fe8012d877764f51
Cookiezby <cookiezby@gmail.com>
2023-10-26 09:16:18 +0900

    revert dictionary extension and update documentation

--- apollo-ios/Sources/Apollo/Dictionary+Helpers.swift
+++ apollo-ios/Sources/Apollo/Dictionary+Helpers.swift
@@ -3,18 +3,3 @@ public extension Dictionary {
     lhs.merge(rhs) { (_, new) in new }
   }
 }
-
-public extension Dictionary where Key == String, Value == Any {
-  var asAnyHashable: [String: AnyHashable] {
-    var result = [String: AnyHashable]()
-
-    for (key, value) in self {
-      if let hashableValue = value as? AnyHashable {
-        result[key] = hashableValue
-      } else if let dictValue = value as? [String: Any] {
-        result[key] = dictValue.asAnyHashable
-      }
-    }
-    return result
-  }
-}
