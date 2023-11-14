e0f72dfcf702016d33ba93767a4951e4c1f218c5
Zach FettersMoore <4425109+BobaFetters@users.noreply.github.com>
2023-11-14 12:30:42 -0500

    Adding XCFramework test project
    -Adding a codegen test project to generate an xcframework from codegen output

---
+++ Tests/TestCodeGenConfigurations/CodegenXCFramework/CodegenXCFramework/MyAPI/Schema/Objects/PetRock.graphql.swift
@@ -0,0 +1,11 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public extension MyAPI.Objects {
+  static let PetRock = ApolloAPI.Object(
+    typename: "PetRock",
+    implementedInterfaces: [MyAPI.Interfaces.Pet.self]
+  )
+}
