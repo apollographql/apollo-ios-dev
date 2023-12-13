143d14917bd69539da41e18121e3d709cc3cf030
Calvin Cestari <calvincestari@users.noreply.github.com>
2023-12-13 12:16:43 -0800

    tests: TestCodegenConfigurations projects should fail on warnings (apollographql/apollo-ios-dev#198)

--- Tests/TestCodeGenConfigurations/EmbeddedInTarget-RelativeAbsolute/test-project.sh
+++ Tests/TestCodeGenConfigurations/EmbeddedInTarget-RelativeAbsolute/test-project.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -e
 
 echo "Testing PackageOne.."
 cd PackageOne
-swift test
+swift test -Xswiftc -warnings-as-errors
 
 echo "Testing PackageTwo.."
 cd ../PackageTwo
-swift test
+swift test -Xswiftc -warnings-as-errors
