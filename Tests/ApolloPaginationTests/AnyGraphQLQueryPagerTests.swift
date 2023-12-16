eec09cf4f7cd6f10e6cf5f86857ac7a90278e57f
Hesham Salman <iron-ham@github.com>
2023-12-16 13:20:13 -0500

    More coverage

--- Tests/ApolloPaginationTests/AnyGraphQLQueryPagerTests.swift
+++ Tests/ApolloPaginationTests/AnyGraphQLQueryPagerTests.swift
@@ -7,6 +7,7 @@ import XCTest
 
 final class AnyGraphQLQueryPagerTests: XCTestCase {
   private typealias Query = MockQuery<Mocks.Hero.FriendsQuery>
+  private typealias ReverseQuery = MockQuery<Mocks.Hero.ReverseFriendsQuery>
 
   private var store: ApolloStore!
   private var server: MockGraphQLServer!
@@ -20,6 +21,100 @@ final class AnyGraphQLQueryPagerTests: XCTestCase {
     networkTransport = MockNetworkTransport(server: server, store: store)
     client = ApolloClient(networkTransport: networkTransport, store: store)
   }
+
+  // MARK: - Test helpers
+
+  private func createPager() -> GraphQLQueryPager<Query, Query> {
+    let initialQuery = Query()
+    initialQuery.__variables = ["id": "2001", "first": 2, "after": GraphQLNullable<String>.null]
+    return GraphQLQueryPager<Query, Query>(
+      client: client,
+      initialQuery: initialQuery,
+      extractPageInfo: { data in
+        switch data {
+        case .initial(let data), .paginated(let data):
+          return CursorBasedPagination.Forward(
+            hasNext: data.hero.friendsConnection.pageInfo.hasNextPage,
+            endCursor: data.hero.friendsConnection.pageInfo.endCursor
+          )
+        }
+      },
+      pageResolver: { pageInfo, direction in
+        guard direction == .next else { return nil }
+        let nextQuery = Query()
+        nextQuery.__variables = [
+          "id": "2001",
+          "first": 2,
+          "after": pageInfo.endCursor,
+        ]
+        return nextQuery
+      }
+    )
+  }
+
+  private func createReversePager() -> GraphQLQueryPager<ReverseQuery, ReverseQuery> {
+    let initialQuery = ReverseQuery()
+    initialQuery.__variables = ["id": "2001", "first": 2, "before": "Y3Vyc29yMw=="]
+    return GraphQLQueryPager<ReverseQuery, ReverseQuery>(
+      client: client,
+      initialQuery: initialQuery,
+      extractPageInfo: { data in
+        switch data {
+        case .initial(let data), .paginated(let data):
+          return CursorBasedPagination.Reverse(
+            hasPrevious: data.hero.friendsConnection.pageInfo.hasPreviousPage,
+            startCursor: data.hero.friendsConnection.pageInfo.startCursor
+          )
+        }
+      },
+      pageResolver: { pageInfo, direction in
+        guard direction == .previous else { return nil }
+        let nextQuery = ReverseQuery()
+        nextQuery.__variables = [
+          "id": "2001",
+          "first": 2,
+          "before": pageInfo.startCursor,
+        ]
+        return nextQuery
+      }
+    )
+
+  }
+
+  // This is due to a timing issue in unit tests only wherein we deinit immediately after waiting for expectations
+  private func ignoringCancellations(error: Error?) {
+    if (error as? PaginationError) == PaginationError.cancellation {
+      return
+    } else {
+      XCTAssertNil(error)
+    }
+  }
+
+  private func fetchFirstPage<T>(pager: AnyGraphQLQueryPager<T>) {
+    let serverExpectation = Mocks.Hero.FriendsQuery.expectationForFirstPage(server: server)
+    pager.fetch()
+    wait(for: [serverExpectation], timeout: 1.0)
+  }
+
+  private func fetchSecondPage<T>(pager: AnyGraphQLQueryPager<T>) throws {
+    let serverExpectation = Mocks.Hero.FriendsQuery.expectationForSecondPage(server: server)
+    pager.loadNext(completion: ignoringCancellations(error:))
+    wait(for: [serverExpectation], timeout: 1.0)
+  }
+
+  private func reverseFetchLastPage<T>(pager: AnyGraphQLQueryPager<T>) {
+    let serverExpectation = Mocks.Hero.ReverseFriendsQuery.expectationForLastItem(server: server)
+    pager.fetch()
+    wait(for: [serverExpectation], timeout: 1.0)
+  }
+
+  private func reverseFetchPreviousPage<T>(pager: AnyGraphQLQueryPager<T>) throws {
+    let serverExpectation = Mocks.Hero.ReverseFriendsQuery.expectationForPreviousItem(server: server)
+    pager.loadPrevious(completion: ignoringCancellations(error:))
+    wait(for: [serverExpectation], timeout: 1.0)
+  }
+
+  // MARK: - Tests
 
   func test_concatenatesPages_matchingInitialAndPaginated() throws {
     struct ViewModel {
@@ -87,10 +182,14 @@ final class AnyGraphQLQueryPagerTests: XCTestCase {
     fetchFirstPage(pager: anyPager)
     wait(for: [initialExpectation], timeout: 1.0)
     XCTAssertEqual(expectedViewModel, "Han Solo")
+    XCTAssertTrue(anyPager.canLoadNext)
+    XCTAssertFalse(anyPager.canLoadPrevious)
 
     try fetchSecondPage(pager: anyPager)
     wait(for: [secondExpectation], timeout: 1.0)
     XCTAssertEqual(expectedViewModel, "Leia Organa")
+    XCTAssertFalse(anyPager.canLoadNext)
+    XCTAssertFalse(anyPager.canLoadPrevious)
   }
 
   @available(iOS 16.0, macOS 13.0, *)
@@ -117,54 +216,42 @@ final class AnyGraphQLQueryPagerTests: XCTestCase {
     await fulfillment(of: [callbackExpectation, secondPageExpectation], timeout: 1)
   }
 
-  // MARK: - Test helpers
-
-  private func createPager() -> GraphQLQueryPager<Query, Query> {
-    let initialQuery = Query()
-    initialQuery.__variables = ["id": "2001", "first": 2, "after": GraphQLNullable<String>.null]
-    return GraphQLQueryPager<Query, Query>(
-      client: client,
-      initialQuery: initialQuery,
-      extractPageInfo: { data in
-        switch data {
-        case .initial(let data), .paginated(let data):
-          return CursorBasedPagination.Forward(
-            hasNext: data.hero.friendsConnection.pageInfo.hasNextPage,
-            endCursor: data.hero.friendsConnection.pageInfo.endCursor
-          )
-        }
-      },
-      pageResolver: { pageInfo, direction in
-        guard direction == .next else { return nil }
-        let nextQuery = Query()
-        nextQuery.__variables = [
-          "id": "2001",
-          "first": 2,
-          "after": pageInfo.endCursor,
-        ]
-        return nextQuery
+  func test_reversePager_loadPrevious() throws {
+    let anyPager = createReversePager().eraseToAnyPager { previous, initial, _ in
+      if let latestPage = previous.last {
+        return latestPage.hero.friendsConnection.friends.first?.name
       }
-    )
-  }
-
-  // This is due to a timing issue in unit tests only wherein we deinit immediately after waiting for expectations
-  private func ignoringCancellations(error: Error?) {
-    if (error as? PaginationError) == PaginationError.cancellation {
-      return
-    } else {
-      XCTAssertNil(error)
+      return initial.hero.friendsConnection.friends.first?.name
     }
-  }
 
-  private func fetchFirstPage<T>(pager: AnyGraphQLQueryPager<T>) {
-    let serverExpectation = Mocks.Hero.FriendsQuery.expectationForFirstPage(server: server)
-    pager.fetch()
-    wait(for: [serverExpectation], timeout: 1.0)
-  }
+    let initialExpectation = expectation(description: "Initial")
+    let secondExpectation = expectation(description: "Second")
+    var expectedViewModel: String?
+    anyPager.subscribe { (result: Result<(String?, UpdateSource), Error>) in
+      switch result {
+      case .success((let viewModel, _)):
+        let oldValue = expectedViewModel
+        expectedViewModel = viewModel
+        if oldValue == nil {
+          initialExpectation.fulfill()
+        } else {
+          secondExpectation.fulfill()
+        }
+      default:
+        XCTFail("Failed to get view models from pager.")
+      }
+    }
 
-  private func fetchSecondPage<T>(pager: AnyGraphQLQueryPager<T>) throws {
-    let serverExpectation = Mocks.Hero.FriendsQuery.expectationForSecondPage(server: server)
-    pager.loadNext(completion: ignoringCancellations(error:))
-    wait(for: [serverExpectation], timeout: 1.0)
+    reverseFetchLastPage(pager: anyPager)
+    wait(for: [initialExpectation], timeout: 1.0)
+    XCTAssertEqual(expectedViewModel, "Han Solo")
+    XCTAssertFalse(anyPager.canLoadNext)
+    XCTAssertTrue(anyPager.canLoadPrevious)
+
+    try reverseFetchPreviousPage(pager: anyPager)
+    wait(for: [secondExpectation], timeout: 1.0)
+    XCTAssertEqual(expectedViewModel, "Luke Skywalker")
+    XCTAssertFalse(anyPager.canLoadNext)
+    XCTAssertFalse(anyPager.canLoadPrevious)
   }
 }
