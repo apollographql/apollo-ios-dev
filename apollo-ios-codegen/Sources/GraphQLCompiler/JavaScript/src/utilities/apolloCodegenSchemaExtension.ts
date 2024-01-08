6157ab8a1a8c1fb4d122c7d4bceac5361a7012a3
Anthony Miller <anthonymdev@gmail.com>
2024-01-08 12:18:34 -0800

    Add @import directive

--- apollo-ios-codegen/Sources/GraphQLCompiler/JavaScript/src/utilities/apolloCodegenSchemaExtension.ts
+++ apollo-ios-codegen/Sources/GraphQLCompiler/JavaScript/src/utilities/apolloCodegenSchemaExtension.ts
@@ -1,5 +1,5 @@
-import { DirectiveDefinitionNode, DocumentNode, Kind, concatAST } from "graphql";
-import { nameNode, stringNode } from "./nodeHelpers";
+import { DirectiveDefinitionNode, DocumentNode, Kind, concatAST, GraphQLString } from "graphql";
+import { nameNode, nonNullNode, stringNode, typeNode } from "./nodeHelpers";
 
 export const directive_apollo_client_ios_localCacheMutation: DirectiveDefinitionNode = {
   kind: Kind.DIRECTIVE_DEFINITION,
@@ -9,16 +9,33 @@ export const directive_apollo_client_ios_localCacheMutation: DirectiveDefinition
   locations: [nameNode("QUERY"), nameNode("MUTATION"), nameNode("SUBSCRIPTION"), nameNode("FRAGMENT_DEFINITION")]
 }
 
+export const directive_import_statement: DirectiveDefinitionNode = {
+  kind: Kind.DIRECTIVE_DEFINITION,
+  description: stringNode("A directive used by the Apollo iOS code generation engine to generate custom import statements in operation or fragment definition files. An import statement to import a module with the name provided in the `module` argument will be added to the generated definition file."),
+  name: nameNode("import"),
+  arguments: [
+    {
+      kind: Kind.INPUT_VALUE_DEFINITION,
+      description: stringNode("The name of the module to import."),
+      name: nameNode("module"),
+      type: nonNullNode(typeNode(GraphQLString))
+    }
+  ],
+  repeatable: true,
+  locations: [nameNode("QUERY"), nameNode("MUTATION"), nameNode("SUBSCRIPTION"), nameNode("FRAGMENT_DEFINITION")]
+}
+
 export const apolloCodegenSchemaExtension: DocumentNode = {
   kind: Kind.DOCUMENT,
   definitions: [
-    directive_apollo_client_ios_localCacheMutation
+    directive_apollo_client_ios_localCacheMutation,
+    directive_import_statement
   ]
 }
 
 export function addApolloCodegenSchemaExtensionToDocument(document: DocumentNode): DocumentNode {
-  return document.definitions.some(definition => 
-    definition.kind == Kind.DIRECTIVE_DEFINITION && 
+  return document.definitions.some(definition =>
+    definition.kind == Kind.DIRECTIVE_DEFINITION &&
     definition.name.value == directive_apollo_client_ios_localCacheMutation.name.value
   ) ?
     document :
