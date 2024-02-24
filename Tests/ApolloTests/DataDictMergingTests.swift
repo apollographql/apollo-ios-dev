872aa3468d6a33de2d8a481b6b1ca5ad903c5d5d
Calvin Cestari <calvin.cestari@gmail.com>
2024-02-23 16:20:41 -0800

    Refactor DataDict merge tests for shared subject

--- Tests/ApolloTests/DataDictMergingTests.swift
+++ Tests/ApolloTests/DataDictMergingTests.swift
@@ -1,218 +1,157 @@
 import XCTest
 @testable import Apollo
 import ApolloAPI
+import ApolloInternalTestHelpers
 import Nimble
 
 class DataDictMergingTests: XCTestCase {
+
+  class Data: MockSelectionSet {
+    class Animal: MockSelectionSet {
+      class Predator: MockSelectionSet { }
+    }
+  }
+
+  let subject = DataDict(
+    data: [
+      "animals": [
+        DataDict(
+          data: [
+            "__typename": "Animal",
+            "name": "Dog"
+          ],
+          fulfilledFragments: [
+            ObjectIdentifier(Data.Animal.self),
+          ]
+        ),
+        DataDict(
+          data: [
+            "__typename": "Animal",
+            "name": "Cat"
+          ],
+          fulfilledFragments: [
+            ObjectIdentifier(Data.Animal.self),
+          ]
+        )
+      ]
+    ],
+    fulfilledFragments: [
+      ObjectIdentifier(Data.self),
+    ]
+  )
 
   // MARK: Errors
 
   func test__merging__givenEmptyPathComponent_throwsError() throws {
     // given
-    let subject = DataDict([
-      "animals": [
-        DataDict([
-          "__typename": "Animal",
-          "name": "Dog"
-        ]),
-        DataDict([
-          "__typename": "Animal",
-          "name": "Cat"
-        ]),
-      ]
-    ])
-
-    let mergeDataDict = DataDict([
-      "key": "value"
-    ])
+    let pathComponents: [PathComponent] = []
 
     // then
     expect(
-      try subject.merging(mergeDataDict, at: [])
+      try self.subject.merging(DataDict.empty(), at: pathComponents)
     ).to(throwError(DataDict.MergeError.emptyMergePath))
   }
 
   func test__merging__givenIndexPathForDataDictType_throwsError() throws {
     // given
-    let subject = DataDict([
-      "animals": [
-        DataDict([
-          "__typename": "Animal",
-          "name": "Dog"
-        ]),
-        DataDict([
-          "__typename": "Animal",
-          "name": "Cat"
-        ]),
-      ]
-    ])
-
-    let mergeDataDict = DataDict([
-      "key": "value"
-    ])
+    let pathComponents: [PathComponent] = [
+      .index(0),
+    ]
 
     // then
     expect(
-      try subject.merging(mergeDataDict, at: [.index(0)])
+      try self.subject.merging(DataDict.empty(), at: [.index(0)])
     ).to(throwError(DataDict.MergeError.invalidPathComponentForDataType(.index(0), "DataDict")))
   }
 
   func test__merging__givenFieldPathForArrayType_throwsError() throws {
     // given
-    let subject = DataDict([
-      "animals": [
-        DataDict([
-          "__typename": "Animal",
-          "name": "Dog"
-        ]),
-        DataDict([
-          "__typename": "Animal",
-          "name": "Cat"
-        ]),
-      ]
-    ])
-
-    let mergeDataDict = DataDict([
-      "key": "value"
-    ])
+    let pathComponents: [PathComponent] = [
+      .field("animals"),
+      .field("first"),
+    ]
 
     // then
     expect(
-      try subject.merging(mergeDataDict, at: [.field("animals"), .field("first")])
+      try self.subject.merging(DataDict.empty(), at: pathComponents)
     ).to(throwError(DataDict.MergeError.invalidPathComponentForDataType(.field("first"), "Array")))
   }
 
   func test__merging__givenInvalidFieldPath_throwsError() throws {
     // given
-    let subject = DataDict([
-      "animals": [
-        DataDict([
-          "__typename": "Animal",
-          "name": "Dog"
-        ]),
-        DataDict([
-          "__typename": "Animal",
-          "name": "Cat"
-        ])
-      ]
-    ])
-
-    let mergeDataDict = DataDict([
-      "__typename": "Animal"
-    ])
+    let pathComponents: [PathComponent] = [
+      .field("nonexistent"),
+    ]
 
     // then
     expect(
-      try subject.merging(mergeDataDict, at: [.field("nonexistent"), .index(0)])
+      try self.subject.merging(DataDict.empty(), at: pathComponents)
     ).to(throwError(DataDict.MergeError.cannotFindPathComponent(.field("nonexistent"))))
   }
 
   func test__merging__givenInvalidIndexPath_throwsError() throws {
     // given
-    let subject = DataDict([
-      "animals": [
-        DataDict([
-          "__typename": "Animal",
-          "name": "Dog"
-        ]),
-        DataDict([
-          "__typename": "Animal",
-          "name": "Cat"
-        ])
-      ]
-    ])
-
-    let mergeDataDict = DataDict([
-      "__typename": "Animal"
-    ])
+    let pathComponents: [PathComponent] = [
+      .field("animals"),
+      .index(3),
+    ]
 
     // then
     expect(
-      try subject.merging(mergeDataDict, at: [.field("animals"), .index(3)])
+      try self.subject.merging(DataDict.empty(), at: pathComponents)
     ).to(throwError(DataDict.MergeError.cannotFindPathComponent(.index(3))))
   }
 
   func test__merging__givenPathToNonDataDictType_throwsError() throws {
     // given
-    let subject = DataDict([
-      "animals": [
-        DataDict([
-          "__typename": "Animal",
-          "name": "Dog"
-        ]),
-        DataDict([
-          "__typename": "Animal",
-          "name": "Cat"
-        ]),
-      ]
-    ])
-
-    let mergeDataDict = DataDict([
-      "key": "value"
-    ])
+    let pathComponents: [PathComponent] = [
+      .field("animals"),
+    ]
 
     // then
     expect(
-      try subject.merging(mergeDataDict, at: [.field("animals")])
+      try self.subject.merging(DataDict.empty(), at: pathComponents)
     ).to(throwError(DataDict.MergeError.incrementalMergeNeedsDataDict))
   }
 
   func test__merging__givenPathToExistingFieldData_withNewValue_throwsError() throws {
     // given
-    let subject = DataDict([
-      "animals": [
-        DataDict([
-          "__typename": "Animal",
-          "name": "Dog"
-        ]),
-        DataDict([
-          "__typename": "Animal",
-          "name": "Cat"
-        ])
-      ]
-    ])
+    let pathComponents: [PathComponent] = [
+      .field("animals"),
+      .index(0),
+    ]
 
-    let mergeDataDict = DataDict([
+    let mergeDataDict = DataDict(
+      data: [
       "__typename": "NewValue"
-    ])
+      ],
+      fulfilledFragments: []
+    )
 
     // then
     expect(
-      try subject.merging(mergeDataDict, at: [.field("animals"), .index(0)])
+      try self.subject.merging(mergeDataDict, at: pathComponents)
     ).to(throwError(DataDict.MergeError.cannotOverwriteFieldData("Animal", "NewValue")))
   }
 
   func test__merging__givenPathToExistingFieldData_withSameValue_doesNotThrowError() throws {
     // given
-    let subject = DataDict([
-      "animals": [
-        DataDict([
-          "__typename": "Animal",
-          "name": "Dog"
-        ]),
-        DataDict([
-          "__typename": "Animal",
-          "name": "Cat"
-        ])
-      ]
-    ])
+    let pathComponents: [PathComponent] = [
+      .field("animals"),
+      .index(0),
+    ]
 
-    let mergeDataDict = DataDict([
-      "__typename": "Animal"
-    ])
+    let mergeDataDict = DataDict(
+      data: [
+        "__typename": "Animal"
+      ],
+      fulfilledFragments: []
+    )
 
     // then
     expect(
-      try subject.merging(mergeDataDict, at: [.field("animals"), .index(0)])
+      try self.subject.merging(mergeDataDict, at: pathComponents)
     ).notTo(throwError())
   }
 
-}
-
-// MARK: - Helpers
-
-extension DataDict {
-  fileprivate init(_ data: [String: AnyHashable]) {
-    self.init(data: data, fulfilledFragments: [])
-  }
 }
