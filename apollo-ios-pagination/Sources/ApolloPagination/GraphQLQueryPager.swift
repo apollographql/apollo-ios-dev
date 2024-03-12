8feda09bef21cbecb02a176b6aefbc31d96c2062
Hesham Salman <iron-ham@github.com>
2024-03-12 13:36:14 -0400

    Unit tests

--- apollo-ios-pagination/Sources/ApolloPagination/GraphQLQueryPager.swift
+++ apollo-ios-pagination/Sources/ApolloPagination/GraphQLQueryPager.swift
@@ -260,6 +260,8 @@ extension GraphQLQueryPager: Equatable where Model: Equatable {
       return leftValue == rightValue && leftSource == rightSource
     case (.failure(let leftError), .failure(let rightError)):
       return leftError.localizedDescription == rightError.localizedDescription
+    case (.none, .none):
+      return true
     default:
       return false
     }
