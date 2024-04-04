35df8d2076d0dab669317784ccd9f9c3a59f63cc
Calvin Cestari <calvin.cestari@gmail.com>
2024-04-04 16:59:57 -0700

    Update versions

--- apollo-ios-codegen/Sources/CodegenCLI/Constants.swift
+++ apollo-ios-codegen/Sources/CodegenCLI/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public enum Constants {
-  public static let CLIVersion: String = "1.9.3"
+  public static let CLIVersion: String = "preview-defer.2"
   static let defaultFilePath: String = "./apollo-codegen-config.json"
 }
