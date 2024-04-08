825d03fa519b452819e9e9e13f3e3a27a612a65e
Jim Isaacs <299156+jimisaacs@users.noreply.github.com>
2024-04-07 23:24:52 -0400

    Replace ApolloStoreSubscriber didChangeKeys with ApolloStore.Activity and new method

--- Tests/ApolloTests/Cache/StoreSubscriptionTests.swift
+++ Tests/ApolloTests/Cache/StoreSubscriptionTests.swift
@@ -2,20 +2,53 @@ import Nimble
 import XCTest
 @testable import Apollo
 
-class StoreSubscriptionTests: XCTestCase {
+open class StoreSubscriptionTests: XCTestCase {
   static let defaultWaitTimeout: TimeInterval = 1
 
   var store: ApolloStore!
 
-  override func setUpWithError() throws {
+  open override func setUpWithError() throws {
     try super.setUpWithError()
     store = ApolloStore()
   }
 
-  override func tearDownWithError() throws {
+  open override func tearDownWithError() throws {
     store = nil
     try super.tearDownWithError()
   }
+
+  // MARK: - Tests
+
+  func testUnsubscribeRemovesSubscriberFromApolloStore() throws {
+    let subscriber = NoopSubscriber()
+
+    store.subscribe(subscriber)
+
+    store.unsubscribe(subscriber)
+
+    expect(self.store.subscribers).toEventually(beEmpty())
+  }
+
+  /// Fufills the provided expectation when all expected keys have been observed.
+  internal class NoopSubscriber: ApolloStoreSubscriber {
+
+    init() {}
+
+    func store(_ store: ApolloStore,
+               didChangeKeys changedKeys: Set<CacheKey>,
+               contextIdentifier: UUID?) {
+      // not implemented, deprecated
+    }
+
+    func store(_ store: Apollo.ApolloStore,
+               activity: Apollo.ApolloStore.Activity,
+               contextIdentifier: UUID?) throws {
+      // not implemented
+    }
+  }
+}
+
+final class StoreSubscriptionSimpleTests: StoreSubscriptionTests {
 
   // MARK: - Tests
 
@@ -39,16 +72,6 @@ class StoreSubscriptionTests: XCTestCase {
     wait(for: [cacheKeyChangeExpectation], timeout: Self.defaultWaitTimeout)
   }
 
-  func testUnsubscribeRemovesSubscriberFromApolloStore() throws {
-    let subscriber = SimpleSubscriber(XCTestExpectation(), [])
-
-    store.subscribe(subscriber)
-
-    store.unsubscribe(subscriber)
-
-    expect(self.store.subscribers).toEventually(beEmpty())
-  }
-
   /// Fufills the provided expectation when all expected keys have been observed.
   internal class SimpleSubscriber: ApolloStoreSubscriber {
     private let expectation: XCTestExpectation
@@ -62,8 +85,71 @@ class StoreSubscriptionTests: XCTestCase {
     func store(_ store: ApolloStore,
                didChangeKeys changedKeys: Set<CacheKey>,
                contextIdentifier: UUID?) {
+      // not implemented, deprecated
+    }
+
+    func store(_ store: Apollo.ApolloStore,
+               activity: Apollo.ApolloStore.Activity,
+               contextIdentifier: UUID?) throws {
+      // To match the old didChangeKeys ApolloStoreSubscriber behavior, only operation on the "did merge" action.
+      guard case .did(perform: .merge, outcome: .changedKeys(let changedKeys)) = activity else {
+        return
+      }
       changeSet.subtract(changedKeys)
       if (changeSet.isEmpty) {
+        expectation.fulfill()
+      }
+    }
+  }
+}
+
+final class StoreSubscriptionAdvancedTests: StoreSubscriptionTests {
+
+  // MARK: - Tests
+
+  func testSubscriberIsNotifiedOfStoreUpdate() throws {
+    let records: RecordSet = [
+      "QUERY_ROOT": [
+        "__typename": "Hero",
+        "name": "Han Solo"
+      ]
+    ]
+    let cacheSubscriberExpectation = XCTestExpectation(description: "Subscriber is notified of all expected activities")
+    let expectedActivitiesSet: Set<ApolloStore.Activity> = [
+        .will(perform: .merge(records: records)),
+        .did(perform: .merge(records: records), outcome: .changedKeys(["QUERY_ROOT.__typename", "QUERY_ROOT.name"]))
+    ]
+    let subscriber = AdvancedSubscriber(cacheSubscriberExpectation, expectedActivitiesSet)
+
+    store.subscribe(subscriber)
+    addTeardownBlock { self.store.unsubscribe(subscriber) }
+
+    store.publish(records: records)
+
+    wait(for: [cacheSubscriberExpectation], timeout: Self.defaultWaitTimeout)
+  }
+
+  /// Fufills the provided expectation when all expected keys have been observed.
+  internal class AdvancedSubscriber: ApolloStoreSubscriber {
+    private let expectation: XCTestExpectation
+    private var activities: Set<ApolloStore.Activity>
+
+    init(_ expectation: XCTestExpectation, _ activities: Set<ApolloStore.Activity>) {
+      self.expectation = expectation
+      self.activities = activities
+    }
+
+    func store(_ store: ApolloStore,
+               didChangeKeys changedKeys: Set<CacheKey>,
+               contextIdentifier: UUID?) {
+      // not implemented, deprecated
+    }
+
+    func store(_ store: Apollo.ApolloStore,
+               activity: Apollo.ApolloStore.Activity,
+               contextIdentifier: UUID?) throws {
+      activities.remove(activity)
+      if (activities.isEmpty) {
         expectation.fulfill()
       }
     }
--- apollo-ios/Sources/Apollo/DataLoader.swift
+++ apollo-ios/Sources/Apollo/DataLoader.swift
@@ -1,5 +1,5 @@
 final class DataLoader<Key: Hashable, Value> {
-  public typealias BatchLoad = (Set<Key>) throws -> [Key: Value]
+  public typealias BatchLoad = (Set<Key>) throws -> [Key: Value]?
   private var batchLoad: BatchLoad
 
   private var cache: [Key: Result<Value?, Error>] = [:]
@@ -29,12 +29,12 @@ final class DataLoader<Key: Hashable, Value> {
     let values = try batchLoad(pendingLoads)
     
     for key in pendingLoads {
-      cache[key] = .success(values[key])
+      cache[key] = .success(values?[key])
     }
     
     pendingLoads.removeAll()
         
-    return values[key]
+    return values?[key]
   }
   
   func removeAll() {
