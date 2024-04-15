43cdc5b8816dd018e20039b53e47e5df0566432c
Hesham Salman <iron-ham@github.com>
2024-04-15 16:11:02 -0400

    Optional debounce

--- apollo-ios/Sources/Apollo/GraphQLQueryWatcher.swift
+++ apollo-ios/Sources/Apollo/GraphQLQueryWatcher.swift
@@ -15,7 +15,7 @@ public final class GraphQLQueryWatcher<Query: GraphQLQuery>: Cancellable, Apollo
 
   private let contextIdentifier = UUID()
   private let context: RequestContext?
-  private let debouncer = Debounce(timeInterval: 0.5)
+  private let debouncer: Debounce?
 
   private class WeakFetchTaskContainer {
     weak var cancellable: Cancellable?
@@ -39,16 +39,26 @@ public final class GraphQLQueryWatcher<Query: GraphQLQuery>: Cancellable, Apollo
   ///   - context: [optional] A context that is being passed through the request chain. Defaults to `nil`.
   ///   - callbackQueue: The queue for the result handler. Defaults to the main queue.
   ///   - resultHandler: The result handler to call with changes.
-  public init(client: ApolloClientProtocol,
-              query: Query,
-              context: RequestContext? = nil,
-              callbackQueue: DispatchQueue = .main,
-              resultHandler: @escaping GraphQLResultHandler<Query.Data>) {
+  public init(
+    client: ApolloClientProtocol,
+    query: Query,
+    context: RequestContext? = nil,
+    callbackQueue: DispatchQueue = .main,
+    debounceTimeInterval: TimeInterval = 0.5,
+    resultHandler: @escaping GraphQLResultHandler<Query.Data>
+  ) {
     self.client = client
     self.query = query
     self.resultHandler = resultHandler
     self.callbackQueue = callbackQueue
     self.context = context
+    if debounceTimeInterval > .zero {
+
+    }
+
+    self.debouncer = debounceTimeInterval > .zero
+      ? Debounce(timeInterval: debounceTimeInterval)
+      : nil
 
     client.store.subscribe(self)
   }
@@ -75,10 +85,18 @@ public final class GraphQLQueryWatcher<Query: GraphQLQuery>: Cancellable, Apollo
           break
         }
 
-        self.debouncer.queue {
-          self.resultHandler(result)
-        }
+        self.debounce(result: result)
       }
+    }
+  }
+
+  func debounce(result: Result<GraphQLResult<Query.Data>, Error>) {
+    if let debouncer {
+      debouncer.queue { [weak self] in
+        self?.resultHandler(result)
+      }
+    } else {
+      resultHandler(result)
     }
   }
 
@@ -94,35 +112,33 @@ public final class GraphQLQueryWatcher<Query: GraphQLQuery>: Cancellable, Apollo
     if
       let incomingIdentifier = contextIdentifier,
       incomingIdentifier == self.contextIdentifier {
-        // This is from changes to the keys made from the `fetch` method above,
-        // changes will be returned through that and do not need to be returned
-        // here as well.
-        return
+      // This is from changes to the keys made from the `fetch` method above,
+      // changes will be returned through that and do not need to be returned
+      // here as well.
+      return
     }
-    
+
     guard let dependentKeys = self.dependentKeys else {
       // This query has nil dependent keys, so nothing that changed will affect it.
       return
     }
-    
+
     if !dependentKeys.isDisjoint(with: changedKeys) {
       // First, attempt to reload the query from the cache directly, in order not to interrupt any in-flight server-side fetch.
       store.load(self.query) { [weak self] result in
         guard let self = self else { return }
-        
+
         switch result {
         case .success(let graphQLResult):
           self.callbackQueue.async { [weak self] in
             guard let self = self else {
               return
             }
-            
+
             self.$dependentKeys.mutate {
               $0 = graphQLResult.dependentKeys
             }
-            self.debouncer.queue {
-              self.resultHandler(result)
-            }
+            self.debounce(result: result)
           }
         case .failure:
           if self.fetching.cachePolicy != .returnCacheDataDontFetch {
