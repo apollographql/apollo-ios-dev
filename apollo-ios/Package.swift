d58b3aac2ed2dfe1bb6fdcd0c19c64b60d197ca6
Anthony Miller <anthonymdev@gmail.com>
2024-07-11 14:02:50 -0700

    Update apollo-ios deployment targets

--- apollo-ios/Package.swift
+++ apollo-ios/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
   name: "Apollo",
   platforms: [
-    .iOS(.v12),
-    .macOS(.v10_14),
-    .tvOS(.v12),
-    .watchOS(.v5),
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+    .watchOS(.v6),
     .visionOS(.v1),
   ],
   products: [
