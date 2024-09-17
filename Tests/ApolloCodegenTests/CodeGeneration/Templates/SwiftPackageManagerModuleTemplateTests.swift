6632a1c87bbe547f06520a070b2cfb68193388c0
Calvin Cestari <calvin.cestari@gmail.com>
2024-09-17 16:47:35 -0700

    Updates test version number check

--- Tests/ApolloCodegenTests/CodeGeneration/Templates/SwiftPackageManagerModuleTemplateTests.swift
+++ Tests/ApolloCodegenTests/CodeGeneration/Templates/SwiftPackageManagerModuleTemplateTests.swift
@@ -34,7 +34,7 @@ class SwiftPackageManagerModuleTemplateTests: XCTestCase {
     buildSubject()
 
     let expected = """
-    // swift-tools-version:5.7
+    // swift-tools-version:5.9
     """
 
     // when
