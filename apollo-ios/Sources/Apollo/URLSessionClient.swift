e8a68afdbef70a38abcffcc38d983a868a575ddc
Åsmund Grammeltvedt <asgramme@microsoft.com>
2024-09-27 15:08:39 +0200

    Be more precise
    The RFC doesn't strictly require crlf to follow the boundary (although
    the relay reference implementation makes that assumption).

--- apollo-ios/Sources/Apollo/URLSessionClient.swift
+++ apollo-ios/Sources/Apollo/URLSessionClient.swift
@@ -298,7 +298,7 @@ open class URLSessionClient: NSObject, URLSessionDelegate, URLSessionTaskDelegat
         return
       }
 
-      let boundaryMarker = "\r\n--\(boundaryString)\r\n"
+      let boundaryMarker = "\r\n--\(boundaryString)"
       guard
         let dataString = String(data: taskData.data, encoding: .utf8),
         let lastBoundaryIndex = dataString.range(of: boundaryMarker, options: .backwards)?.upperBound,
