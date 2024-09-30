6a625dbc019ab2c6166dbf85678e703f19d0b779
Anthony Miller <anthonymdev@gmail.com>
2024-09-30 15:52:50 -0700

    Remove server integration tests (apollographql/apollo-ios-dev#497)

--- scripts/install-node-v12.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#touch $GITHUB_ENV
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-echo 'export NVM_DIR="$HOME/.nvm"<<EOF' >> $GITHUB_ENV
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"<<EOF' >> $GITHUB_ENV
-echo nvm install v12.22.10 >> $GITHUB_ENV
-echo nvm use v18.15.0 >> $GITHUB_ENV
