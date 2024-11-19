07c8add38cf93b073b566d34281c03826ca444de
gh-action-runner <runner@Mac-1731980968306.local>
2024-11-19 01:57:10 +0000

    Squashed 'apollo-ios-codegen/' changes from f0c5dc79..759edca9
    759edca9 ci: Minor job improvements (apollographql/apollo-ios-dev#541)
    
    git-subtree-dir: apollo-ios-codegen
    git-subtree-split: 759edca95ec38433bb3a6ec509befa82a784ad50

--- Sources/GraphQLCompiler/JavaScript/auto_rollup.sh
+++ Sources/GraphQLCompiler/JavaScript/auto_rollup.sh
@@ -3,7 +3,7 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 output_file="$SCRIPT_DIR/../ApolloCodegenFrontendBundle.swift"
-cd "$SCRIPT_DIR" && npm run build
+cd "$SCRIPT_DIR" && npm install && npm run build
 minJS=$(cat "$SCRIPT_DIR/dist/ApolloCodegenFrontend.bundle.js")
 printf "%s%s%s" "let ApolloCodegenFrontendBundle: String = #\"" "$minJS" "\"#" > "$output_file"
 exit 0
