85b6c41098269b1d259b6515e3a626e7a4294c43
Calvin Cestari <146856+calvincestari@users.noreply.github.com>
2025-01-23 16:14:44 -0800

    chore: test codegen configuration project cleanup (apollographql/apollo-ios-dev#585)

--- Tests/TestCodeGenConfigurations/CodegenXCFramework/CodegenXCFramework/MyAPI/Schema/Interfaces/Pet.graphql.swift
+++
@@ -1,8 +0,0 @@
-// @generated
-// This file was automatically generated and should not be edited.
-
-import ApolloAPI
-
-public extension MyAPI.Interfaces {
-  static let Pet = ApolloAPI.Interface(name: "Pet")
-}
