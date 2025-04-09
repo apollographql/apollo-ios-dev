ec9d636010ee6fa891409d9ff57610716176a032
Anthony Miller <anthonymdev@gmail.com>
2025-04-08 17:52:35 -0700

    WIP: Network fetch returns data as chunks

--- apollo-ios/Sources/Apollo/Interceptors/ResponseCodeInterceptor.swift
+++ apollo-ios/Sources/Apollo/Interceptors/ResponseCodeInterceptor.swift
@@ -7,25 +7,24 @@ import ApolloAPI
 public struct ResponseCodeInterceptor: ApolloInterceptor {
 
   public var id: String = UUID().uuidString
-  
+
   public struct ResponseCodeError: Error, LocalizedError {
     let response: HTTPURLResponse
+    let responseChunk: Data
 
     public var errorDescription: String? {
       return "Received a \(response.statusCode) error."
     }
-    
+
     public var graphQLError: GraphQLError? {
-      switch self {
-      case .invalidResponseCode(_, let rawData):
-        if let jsonRawData = rawData,
-           let jsonData = try? (JSONSerialization.jsonObject(with: jsonRawData, options: .allowFragments) as! JSONValue),
-           let jsonObject = try? JSONObject(_jsonValue: jsonData)
-        {
-          return GraphQLError(jsonObject)
-        }
-        return nil
+      if let jsonValue = try? (JSONSerialization.jsonObject(
+          with: responseChunk,
+          options: .allowFragments) as! JSONValue),
+         let jsonObject = try? JSONObject(_jsonValue: jsonValue)
+      {
+        return GraphQLError(jsonObject)
       }
+      return nil
     }
   }
   
@@ -35,16 +34,16 @@ public struct ResponseCodeInterceptor: ApolloInterceptor {
   public func intercept<Operation: GraphQLOperation>(
     request: HTTPRequest<Operation>,
     next: NextInterceptorFunction<Operation>
-  ) async throws -> InterceptorResult<Operation> {
-    let result = try await next(request)
+  ) async throws -> InterceptorResultStream<Operation> {
+    return try await next(request).map { result in
 
-    guard result.response.isSuccessful == true else {
-      throw ResponseCodeError.invalidResponseCode(
-        response: result.response,
-        rawData: response?.rawData
-      )
+      guard result.response.isSuccessful == true else {
+        throw ResponseCodeError(
+          response: result.response,
+          responseChunk: result.rawResponseChunk
+        )
+      }
+      return result
     }
-
-    return result
   }
 }
