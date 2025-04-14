e98d5fd05ed4d87429dce913bdb44ff9f781ba55
Anthony Miller <anthonymdev@gmail.com>
2025-04-14 16:31:24 -0700

    WIP: Fixing Interceptor unit tests

--- Tests/ApolloInternalTestHelpers/MockHTTPResponse.swift
+++ Tests/ApolloInternalTestHelpers/MockHTTPResponse.swift
@@ -2,23 +2,6 @@ import Apollo
 import ApolloAPI
 import Foundation
 
-extension HTTPResponse {
-  public static func mock(
-    statusCode: Int = 200,
-    headerFields: [String : String] = [:],
-    data: Data = Data()
-  ) -> HTTPResponse {
-    return HTTPResponse(
-      response: .mock(
-        statusCode: statusCode,
-        headerFields: headerFields
-      ),
-      rawData: data,
-      parsedResponse: nil
-    )
-  }
-}
-
 extension HTTPURLResponse {
   public static func mock(
     url: URL = TestURL.mockServer.url,
