d8e775d26b6eafb0d7c77d87f3eebdf4f9176f8e
Anthony Miller <anthonymdev@gmail.com>
2025-05-14 12:21:56 -0700

    Update Multipart parsing to parse Data not string
    Removes extra String serialization and deserialization and improves performance

--- Tests/ApolloTests/MultipartFormData+Testing.swift
+++ Tests/ApolloTests/MultipartFormData+Testing.swift
@@ -7,8 +7,9 @@ extension MultipartFormData {
     let encodedData = try self.encode()
     let string = String(bytes: encodedData, encoding: .utf8)!
 
+    let crlf = String(data: MultipartResponseParsing.CRLF, encoding: .utf8)!
     // Replacing CRLF with new line as string literals uses new lines
-    return string.replacingOccurrences(of: MultipartFormData.CRLF, with: "\n")
+    return string.replacingOccurrences(of: crlf, with: "\n")
   }
 }
 
