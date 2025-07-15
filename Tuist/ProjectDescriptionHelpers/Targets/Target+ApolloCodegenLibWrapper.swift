0acbfe02af58452388432c0314f3e691b59aeb1b
Anthony Miller <anthonymdev@gmail.com>
2025-07-15 13:57:26 -0700

    Update Tuist to 4.55.6

--- Tuist/ProjectDescriptionHelpers/Targets/Target+ApolloCodegenLibWrapper.swift
+++ Tuist/ProjectDescriptionHelpers/Targets/Target+ApolloCodegenLibWrapper.swift
@@ -1,22 +1,22 @@
 import ProjectDescription
 
 extension Target {
-    
-    public static func apolloCodegenLibWrapperFramework() -> Target {
-        let target: ApolloTarget = .apolloCodegenLibWrapper
-        
-        return Target(
-            name: target.name,
-            destinations: target.destinations,
-            product: .framework,
-            bundleId: "com.apollographql.\(target.name.lowercased())",
-            deploymentTargets: target.deploymentTargets,
-            infoPlist: .file(path: "Tests/\(target.name)/Info.plist"),
-            dependencies: [
-                .package(product: "ApolloCodegenLib")
-            ],
-            settings: .forTarget(target)
-        )
-    }
-    
+
+  public static func apolloCodegenLibWrapperFramework() -> Target {
+    let target: ApolloTarget = .apolloCodegenLibWrapper
+
+    return .target(
+      name: target.name,
+      destinations: target.destinations,
+      product: .framework,
+      bundleId: "com.apollographql.\(target.name.lowercased())",
+      deploymentTargets: target.deploymentTargets,
+      infoPlist: .file(path: "Tests/\(target.name)/Info.plist"),
+      dependencies: [
+        .package(product: "ApolloCodegenLib")
+      ],
+      settings: .forTarget(target)
+    )
+  }
+
 }
--- Tuist/ProjectDescriptionHelpers/Targets/Target+ApolloPerformanceTests.swift
+++ Tuist/ProjectDescriptionHelpers/Targets/Target+ApolloPerformanceTests.swift
@@ -1,54 +1,53 @@
 import ProjectDescription
 
 extension Target {
-    
-    public static func apolloPerformanceTests() -> Target {
-        let target: ApolloTarget = .apolloPerformanceTests
-        
-        return Target(
-            name: target.name,
-            destinations: target.destinations,
-            product: .unitTests,
-            bundleId: "com.apollographql.\(target.name.lowercased())",
-            deploymentTargets: target.deploymentTargets,
-            infoPlist: .file(path: "Tests/\(target.name)/Info.plist"),
-            sources: [
-                "Tests/\(target.name)/**",
-            ],
-            resources: ResourceFileElements(
-                resources: [
-                    .glob(pattern: "Tests/\(target.name)/Responses/**/*.json")
-                ]
-            ),
-            dependencies: [
-                .target(name: ApolloTarget.apolloInternalTestHelpers.name),
-                .target(name: ApolloTarget.animalKingdomAPI.name),
-                .target(name: ApolloTarget.gitHubAPI.name),
-                .package(product: "Apollo"),
-                .package(product: "Nimble")
-            ],
-            settings: .forTarget(target)
-        )
-    }
-    
+
+  public static func apolloPerformanceTests() -> Target {
+    let target: ApolloTarget = .apolloPerformanceTests
+
+    return .target(
+      name: target.name,
+      destinations: target.destinations,
+      product: .unitTests,
+      bundleId: "com.apollographql.\(target.name.lowercased())",
+      deploymentTargets: target.deploymentTargets,
+      infoPlist: .file(path: "Tests/\(target.name)/Info.plist"),
+      sources: [
+        "Tests/\(target.name)/**"
+      ],
+      resources: .resources([
+        .glob(pattern: "Tests/\(target.name)/Responses/**/*.json")
+      ]),
+      dependencies: [
+        .target(name: ApolloTarget.apolloInternalTestHelpers.name),
+        .target(name: ApolloTarget.animalKingdomAPI.name),
+        .target(name: ApolloTarget.gitHubAPI.name),
+        .package(product: "Apollo"),
+        .package(product: "Nimble"),
+      ],
+      settings: .forTarget(target)
+    )
+  }
+
 }
 
 extension Scheme {
-    
-    public static func apolloPerformanceTests() -> Scheme {
-        let target: ApolloTarget = .apolloPerformanceTests
-        
-        return Scheme(
-            name: target.name,
-            buildAction: .buildAction(targets: [
-                TargetReference(projectPath: nil, target: target.name)
-            ]),
-            testAction: .testPlans([
-                ApolloTestPlan.performanceTest.path
-            ],
-            configuration: .performanceTesting
-            )
-        )
-    }
-    
+
+  public static func apolloPerformanceTests() -> Scheme {
+    let target: ApolloTarget = .apolloPerformanceTests
+
+    return .scheme(
+      name: target.name,
+      buildAction: .buildAction(targets: [
+        TargetReference.target(target.name)
+      ]),
+      testAction: .testPlans(
+        [
+          ApolloTestPlan.performanceTest.path
+        ],
+        configuration: .performanceTesting
+      )
+    )
+  }
+
 }
