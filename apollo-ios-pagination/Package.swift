f0d2e1cdff681dd11b9e25f2f70326574b0cafe3
Anthony Miller <anthonymdev@gmail.com>
2025-07-16 14:35:59 -0700

    Update SPM Packages to swift-tools-version 6.1

--- apollo-ios-pagination/Package.swift
+++ apollo-ios-pagination/Package.swift
@@ -1,7 +1,4 @@
-// swift-tools-version:5.9
-//
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-// Swift 5.9 is available from Xcode 15.0.
+// swift-tools-version:6.1
 
 import PackageDescription
 
