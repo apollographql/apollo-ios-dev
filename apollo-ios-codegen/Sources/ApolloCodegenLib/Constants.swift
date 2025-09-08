30bcb53235af19fe26cfde1695735ed50a1bfaf8
Anthony Miller <anthonymdev@gmail.com>
2025-09-08 11:24:22 -0700

    Updated version numbers

--- apollo-ios-codegen/Sources/ApolloCodegenLib/Constants.swift
+++ apollo-ios-codegen/Sources/ApolloCodegenLib/Constants.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 public enum Constants {
-  public static let CodegenVersion: String = "2.0.0-beta-1"
+  public static let CodegenVersion: String = "2.0.0-beta-2"
 }
--- apollo-ios-codegen/Sources/CodegenCLI/Constants.swift
+++ apollo-ios-codegen/Sources/CodegenCLI/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public enum Constants {
-  public static let CLIVersion: String = "2.0.0-beta-1"
+  public static let CLIVersion: String = "2.0.0-beta-2"
   static let defaultFilePath: String = "./apollo-codegen-config.json"
 }
