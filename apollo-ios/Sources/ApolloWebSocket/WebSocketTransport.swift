75b1b8f1a0a4de24fc83397f5ef749f2284ad65e
MultiScott <scott@please.ai>
2025-09-18 16:03:59 -0700

    Update WebSocketTransport Docs

--- apollo-ios/Sources/ApolloWebSocket/WebSocketTransport.swift
+++ apollo-ios/Sources/ApolloWebSocket/WebSocketTransport.swift
@@ -5,8 +5,9 @@ import ApolloAPI
 public final class WebSocketTransport: SubscriptionNetworkTransport {
 
   public enum Error: Swift.Error {
-    /// WebSocketTransport has not yet been implemented for Apollo iOS 2.0. This will be implemented in a future
-    /// release.
+    /// WebSocketTransport has not yet been implemented for Apollo iOS 2.0. This will be implemented
+    /// in a 2.1 update as soon as we can after 2.0 is out. Subscriptions over HTTP will work via the
+    /// RequestChainNetworkTransport in 2.0 out of the box in the meantime. 
     case notImplemented
   }
 
