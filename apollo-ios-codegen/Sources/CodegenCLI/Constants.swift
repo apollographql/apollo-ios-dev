fd9cb8f153933f5712d18cec724d0942d371e312
Anthony Miller <anthonymdev@gmail.com>
2025-09-29 11:25:38 -0700

    Prepare beta 3 release

--- apollo-ios-codegen/Sources/CodegenCLI/Constants.swift
+++ apollo-ios-codegen/Sources/CodegenCLI/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public enum Constants {
-  public static let CLIVersion: String = "2.0.0-beta-2"
+  public static let CLIVersion: String = "2.0.0-beta-3"
   static let defaultFilePath: String = "./apollo-codegen-config.json"
 }
