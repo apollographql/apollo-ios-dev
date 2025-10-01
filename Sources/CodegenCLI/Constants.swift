f896204ea2679d04c56411738a8ebe99ce187f0b
gh-action-runner <runner@iad20-fj923-f5528323-0606-4a1e-8910-d4448eacd105-A6C6A2FC240F.local>
2025-10-01 20:53:41 +0000

    Squashed 'apollo-ios-codegen/' changes from 68aee5940..a4c67ce25
    a4c67ce25 Release 2.0.0 (apollographql/apollo-ios-dev#781)
    
    git-subtree-dir: apollo-ios-codegen
    git-subtree-split: a4c67ce25d6548356a32f71e973790e24ff95eff

--- Sources/CodegenCLI/Constants.swift
+++ Sources/CodegenCLI/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public enum Constants {
-  public static let CLIVersion: String = "2.0.0-beta-3"
+  public static let CLIVersion: String = "2.0.0"
   static let defaultFilePath: String = "./apollo-codegen-config.json"
 }
