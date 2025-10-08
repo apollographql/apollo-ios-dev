feee78fb65f93f2f348a5b7c198a9054b613755e
Anthony Miller <anthonymdev@gmail.com>
2025-10-08 15:04:16 -0700

    Include SPIs only if access modifier is “public”

--- apollo-ios-codegen/Sources/ApolloCodegenLib/Templates/AccessControlRenderer.swift
+++ apollo-ios-codegen/Sources/ApolloCodegenLib/Templates/AccessControlRenderer.swift
@@ -15,29 +15,37 @@ struct AccessControlRenderer {
     case member
   }
 
-  private let computedModifier: String
+  private let accessModifier: ApolloCodegenConfiguration.AccessModifier?
 
   init(
     target: TemplateTarget,
     config: ApolloCodegenConfiguration,
     scope: Scope
   ) {
-    self.computedModifier = Self.accessControlModifier(target: target, config: config, scope: scope)
+    self.accessModifier = Self.accessControlModifier(target: target, config: config, scope: scope)
   }
 
   func accessControl(withSPIs spis: [SPI] = []) -> String {
-    var string = computedModifier
+    var string = accessModifier?.swiftString ?? ""
+
+    guard shouldIncludeSPI else { return string }
+
     for spi in spis {
       string = "@_spi(\(spi)) " + string
     }
     return string
   }
 
+  /// SPI should only be used on public declarations. Internal declarations must omit them.
+  private var shouldIncludeSPI: Bool {
+    accessModifier == .public
+  }
+
   private static func accessControlModifier(
     target: TemplateTarget,
     config: ApolloCodegenConfiguration,
     scope: Scope
-  ) -> String {
+  ) -> ApolloCodegenConfiguration.AccessModifier? {
     switch target {
     case .moduleFile, .schemaFile: return schemaAccessControlModifier(scope, config)
     case .operationFile: return operationAccessControlModifier(scope, config)
@@ -48,50 +56,50 @@ struct AccessControlRenderer {
   private static func schemaAccessControlModifier(
     _ scope: Scope,
     _ config: ApolloCodegenConfiguration,
-  ) -> String {
+  ) -> ApolloCodegenConfiguration.AccessModifier? {
     switch (config.output.schemaTypes.moduleType, scope) {
     case (.embeddedInTarget, .parent):
-      return ""
+      return nil
     case (.embeddedInTarget(_, .public), .namespace),
       (.embeddedInTarget(_, .public), .member):
-      return ApolloCodegenConfiguration.AccessModifier.public.swiftString
+      return .public
     case (.embeddedInTarget(_, .internal), .namespace),
       (.embeddedInTarget(_, .internal), .member):
-      return ApolloCodegenConfiguration.AccessModifier.internal.swiftString
+      return .internal
     case (.swiftPackage, _),
       (.other, _):
-      return ApolloCodegenConfiguration.AccessModifier.public.swiftString
+      return .public
     }
   }
 
   private static func operationAccessControlModifier(
     _ scope: Scope,
     _ config: ApolloCodegenConfiguration,
-  ) -> String {
+  ) -> ApolloCodegenConfiguration.AccessModifier? {
     switch (config.output.operations, scope) {
     case (.inSchemaModule, _):
       return schemaAccessControlModifier(scope, config)
     case (.absolute(_, .public), _),
       (.relative(_, .public), _):
-      return ApolloCodegenConfiguration.AccessModifier.public.swiftString
+      return .public
     case (.absolute(_, .internal), _),
       (.relative(_, .internal), _):
-      return ApolloCodegenConfiguration.AccessModifier.internal.swiftString
+      return .internal
     }
   }
 
   private static func testMockAccessControlModifier(
     _ scope: Scope,
     _ config: ApolloCodegenConfiguration,
-  ) -> String {
+  ) -> ApolloCodegenConfiguration.AccessModifier? {
     switch (config.output.testMocks, scope) {
     case (.none, _):
-      return ""
+      return nil
     case (.absolute(_, .internal), _):
-      return ApolloCodegenConfiguration.AccessModifier.internal.swiftString
+      return .internal
     case (.swiftPackage, _),
       (.absolute(_, .public), _):
-      return ApolloCodegenConfiguration.AccessModifier.public.swiftString
+      return .public
     }
   }
 
