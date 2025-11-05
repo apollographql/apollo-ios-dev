bd34ff9631244eb409d9cb8805c8d1c507085b57
Calvin Cestari <146856+calvincestari@users.noreply.github.com>
2025-11-05 15:42:39 -0800

    ci: Codegen test scripts should halt job on failure (apollographql/apollo-ios-dev#825)

--- Tests/TestCodeGenConfigurations/CodegenXCFramework/test-project.sh
+++ Tests/TestCodeGenConfigurations/CodegenXCFramework/test-project.sh
@@ -5,6 +5,6 @@ set -o pipefail  # Fail if any command in a pipeline fails
 # Clean up previous build artifacts
 rm -rf ./build
 
-xcodebuild archive -configuration Release -project "CodegenXCFramework.xcodeproj" -scheme "CodegenXCFramework" -destination 'generic/platform=macOS' -archivePath "./build/macOS.xcarchive" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=NO | xcbeautify
+xcodebuild archive -configuration Release -project "CodegenXCFramework.xcodeproj" -scheme "CodegenXCFramework" -destination 'generic/platform=macOS' -archivePath "./build/macOS.xcarchive" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=NO | xcbeautify --is-ci
 
-xcodebuild -create-xcframework -allow-internal-distribution -output ./build/CodegenXCFramework.xcframework -framework ./build/macOS.xcarchive/Products/Library/Frameworks/CodegenXCFramework.framework | xcbeautify
+xcodebuild -create-xcframework -allow-internal-distribution -output ./build/CodegenXCFramework.xcframework -framework ./build/macOS.xcarchive/Products/Library/Frameworks/CodegenXCFramework.framework | xcbeautify --is-ci
