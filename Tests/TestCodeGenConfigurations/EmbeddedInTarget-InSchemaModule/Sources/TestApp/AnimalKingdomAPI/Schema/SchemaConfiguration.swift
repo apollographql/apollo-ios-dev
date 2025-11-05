b9662f2456c17b7ba5d46c29cac50e6ca4ec62df
Anthony Miller <anthonymdev@gmail.com>
2025-11-05 14:49:48 -0800

    Minor fix

--- Tests/TestCodeGenConfigurations/EmbeddedInTarget-InSchemaModule/Sources/TestApp/AnimalKingdomAPI/Schema/SchemaConfiguration.swift
+++ Tests/TestCodeGenConfigurations/EmbeddedInTarget-InSchemaModule/Sources/TestApp/AnimalKingdomAPI/Schema/SchemaConfiguration.swift
@@ -10,6 +10,6 @@ import ApolloAPI
 enum SchemaConfiguration: ApolloAPI.SchemaConfiguration {
   static func cacheKeyInfo(for type: ApolloAPI.Object, object: ApolloAPI.ObjectData) -> CacheKeyInfo? {
     // Implement this function to configure cache key resolution for your schema types.
-    return nil
+    return try? CacheKeyInfo(jsonValue: object["id"])
   }
 }
