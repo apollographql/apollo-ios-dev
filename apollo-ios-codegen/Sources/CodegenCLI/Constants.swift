abe37e0b0762f1d42c49a1a85bd49f4153b1dda1
gh-action-runner <runner@iad20-gt1022-2a230b9c-e088-4165-b2ee-579cabe88df2-E6E27A83F3F5.local>
2025-11-05 20:57:20 +0000

    Updated version numbers

--- apollo-ios-codegen/Sources/CodegenCLI/Constants.swift
+++ apollo-ios-codegen/Sources/CodegenCLI/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public enum Constants {
-  public static let CLIVersion: String = "2.0.2"
+  public static let CLIVersion: String = "2.0.3"
   static let defaultFilePath: String = "./apollo-codegen-config.json"
 }
