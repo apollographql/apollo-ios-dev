a76c26eb68707fe141accb06d41371a8fd2a8b92
apollo-bot2 <50160705+apollo-bot2@users.noreply.github.com>
2026-01-15 11:02:51 -0800

    Release 2.0.5 (apollographql/apollo-ios-dev#876)

--- apollo-ios-codegen/Sources/CodegenCLI/Constants.swift
+++ apollo-ios-codegen/Sources/CodegenCLI/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public enum Constants {
-  public static let CLIVersion: String = "2.0.4"
+  public static let CLIVersion: String = "2.0.5"
   static let defaultFilePath: String = "./apollo-codegen-config.json"
 }
