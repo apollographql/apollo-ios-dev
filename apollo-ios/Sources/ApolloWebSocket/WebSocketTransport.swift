5c1b116ac8cd0b685a7cf9caa4a4c6cbd9958808
Anthony Miller <anthonymdev@gmail.com>
2026-01-28 11:46:29 -0800

    Start setting up WebSocketConnection

--- apollo-ios/Sources/ApolloWebSocket/WebSocketTransport.swift
+++ apollo-ios/Sources/ApolloWebSocket/WebSocketTransport.swift
@@ -1,8 +1,8 @@
-import Foundation
 import Apollo
 import ApolloAPI
+import Foundation
 
-public final class WebSocketTransport: SubscriptionNetworkTransport {
+public actor WebSocketTransport: SubscriptionNetworkTransport {
 
   public enum Error: Swift.Error {
     /// WebSocketTransport has not yet been implemented for Apollo iOS 2.0. This will be implemented in a future
@@ -10,13 +10,16 @@ public final class WebSocketTransport: SubscriptionNetworkTransport {
     case notImplemented
   }
 
-  private var connection: any WebSocketConnection
+  private var connection: WebSocketConnection?
 
-  public init(connection: any WebSocketConnection) {
-    self.connection = connection
+  public let session: WebSocketURLSession
+
+  public init(session: WebSocketURLSession) {
+    self.session = session
+//    self.connection = WebSocketConnection()
   }
 
-  public func send<Subscription: GraphQLSubscription>(
+  nonisolated public func send<Subscription: GraphQLSubscription>(
     subscription: Subscription,
     fetchBehavior: Apollo.FetchBehavior,
     requestConfiguration: Apollo.RequestConfiguration
@@ -26,15 +29,59 @@ public final class WebSocketTransport: SubscriptionNetworkTransport {
 
 }
 
-public protocol WebSocketConnection: Actor {
+public final class WebSocketConnection: Sendable {
 
-  func openConnection() async throws
+  //  enum State {
+  //    case notStarted
+  //    case connecting
+  //    case connected
+  //    case disconnected
+  //  }
 
-  func sendOperation(with: Any) throws -> AsyncThrowingStream<JSONObject, any Swift.Error>
+  //  private unowned let transport: WebSocketTransport
+  private let webSocketTask: URLSessionWebSocketTask
+
+  init(task: URLSessionWebSocketTask) {
+    self.webSocketTask = task
+  }
+
+  deinit {
+    self.webSocketTask.cancel(with: .goingAway, reason: nil)
+  }
+
+  func openConnection() async throws -> AsyncThrowingStream<URLSessionWebSocketTask.Message, any Swift.Error> {
+    webSocketTask.resume()
+    return AsyncThrowingStream { [weak self] in
+      guard let self else { return nil }
+
+      try Task.checkCancellation()
+
+      let message = try await self.webSocketTask.receive()
+
+      return Task.isCancelled ? nil : message
+    }
+  }
+
+  func send(with: Any) throws {
+
+  }
 
 }
 
-public protocol SubscriptionURLSession: Sendable {
+public protocol GraphQLWebSocketRequest<Operation>: Sendable {
+  associatedtype Operation: GraphQLOperation
 
-//  func
+  /// The GraphQL Operation to execute
+  var operation: Operation { get set }
+
+  /// The ``FetchBehavior`` to use for this request.
+  /// Determines if fetching will include cache/network.
+  var fetchBehavior: FetchBehavior { get set }
+
+  /// Determines if the results of a network fetch should be written to the local cache.
+  var writeResultsToCache: Bool { get set }
+
+  /// The timeout interval specifies the limit on the idle interval allotted to a request in the process of
+  /// loading. This timeout interval is measured in seconds.
+  var requestTimeout: TimeInterval? { get set }
 }
